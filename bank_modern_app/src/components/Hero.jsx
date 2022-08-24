a1708b9512d53f6cc8c3da072f1f845f818af8da
koolung <koolung432@gmail.com>
2022-08-24 00:56:50 -0300

    Hero section refinement

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -5,9 +5,6 @@ import ContactUsNow from "./ContactUsNow";
 
 const Hero = () => (
   <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-    <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
-    <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient" />
-    <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
     <div
       className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}
     >
